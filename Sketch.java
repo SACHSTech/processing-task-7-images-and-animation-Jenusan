386582f0e5e853cff5c407cc2b4099e1a983fef7
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2022-04-13 18:01:46 -0400

    FINAL

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,21 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  float dvdposx;
+  float dvdposy;
+  float Xspeed = 15;
+  float Yspeed = 0;
+  float wave = 0;
+  int move = 10;
+  
+
+
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -16,20 +23,45 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255,255,255);
+    PImage fabroa = loadImage("Fabroa.jpg");
+    fabroa.resize(width, height);
+    image(fabroa, 0, 0);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    PImage fabroa = loadImage("Fabroa.jpg");
+    fabroa.resize(width, height);
+    image(fabroa, 0, 0);
+
+    wave += (Math.PI/6);
+    Yspeed = (float)(Math.sin(wave)) * 3;
+
+    dvdposx += Xspeed;
+    dvdposy += move + Yspeed;
+
+    
+    if(dvdposx > width - (width / 3) || dvdposx < 0){
+      Xspeed *= -1;
+    }
+    if(dvdposy < 0 || dvdposy > height - (height /6)){
+      Yspeed *= -1;
+      move *= -1;
+    }
+
+
+
+
+
+
+    PImage dvdLogo = loadImage("DVD.png");
+    dvdLogo.resize(width / 3, height / 6);
+    image(dvdLogo, dvdposx, dvdposy);
+
   }
   
   // define other methods down here.
