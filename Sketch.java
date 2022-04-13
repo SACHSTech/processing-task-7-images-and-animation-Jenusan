ac41f3fd624452e5df8bde386e3d1c702bd8c67b
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2022-04-13 18:14:31 -0400

    DONE

--- Sketch.java
+++ Sketch.java
@@ -1,3 +1,9 @@
+/**
+ * A code to display a picture of Mr fabroa in the backround while a dvd logo bounces around the screen 
+ * in a wave pattern 
+ * Author: @Jenusan Y
+ */
+
 import processing.core.PApplet;
 import processing.core.PImage;
 
@@ -7,8 +13,8 @@ public class Sketch extends PApplet {
   float dvdposx;
   float dvdposy;
   float Xspeed = 15;
-  float Yspeed = 0;
-  float wave = 0;
+  float Yspeed;
+  float wave;
   int move = 10;
   
 
@@ -23,7 +29,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(255,255,255);
+    // sets up backround image to be Mr.Fabroa 
     PImage fabroa = loadImage("Fabroa.jpg");
     fabroa.resize(width, height);
     image(fabroa, 0, 0);
@@ -33,18 +39,20 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-
+    // replaces Backround photo every run through so trail is not left behind
     PImage fabroa = loadImage("Fabroa.jpg");
     fabroa.resize(width, height);
     image(fabroa, 0, 0);
 
+    // processes sinocoidal fucntion so that the DVD logo can move in a wave motion
     wave += (Math.PI/6);
     Yspeed = (float)(Math.sin(wave)) * 3;
 
+    // changes the X and Y position of the image each time the method is ran though, so that image can move
     dvdposx += Xspeed;
     dvdposy += move + Yspeed;
-
     
+    // reverses direction of motion of animation once a barrier is reached 
     if(dvdposx > width - (width / 3) || dvdposx < 0){
       Xspeed *= -1;
     }
@@ -52,12 +60,8 @@ public class Sketch extends PApplet {
       Yspeed *= -1;
       move *= -1;
     }
-
-
-
-
-
-
+    
+    // prints DVD logo on screen using all past conditions and values
     PImage dvdLogo = loadImage("DVD.png");
     dvdLogo.resize(width / 3, height / 6);
     image(dvdLogo, dvdposx, dvdposy);
